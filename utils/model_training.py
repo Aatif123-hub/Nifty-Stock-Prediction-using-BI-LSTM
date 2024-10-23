a081d1ed87b35a499c92b0b3fdb3cab2de2d80c2
Aatif123-hub <71110011+Aatif123-hub@users.noreply.github.com>
2024-10-23 18:53:00 -0500

    Added model training file

---
+++ utils/model_training.py
@@ -0,0 +1,21 @@
+import math
+from sklearn.metrics import mean_absolute_error
+
+def train_model(model, x_train, y_train, x_test, y_test, epochs=15, batch_size=64):
+    try:
+      model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size, verbose=1)
+      train_predict = model.predict(x_train)
+      test_predict = model.predict(x_test)
+
+    except Exception as e:
+       raise Exception(f"Cannot train the model. Error:{e}")
+    return train_predict, test_predict
+
+def calculate_errors(y_true, y_pred):
+    try:
+      rmse = math.sqrt(mean_absolute_error(y_true, y_pred))
+    
+    except Exception as e:
+      raise Exception(f"Cannot calculate the error. Error:{e}")
+    
+    return rmse
